f90b59f04884c93c3e12de7500c97b4e949efe1a
shubhangisanu <98093128+shubhangisanu@users.noreply.github.com>
2022-04-13 21:04:35 +0530

    FrontEnd

---
+++ src/component/Aboutus.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DietHome from "./DietHome";
+import GetAllPackages from "./GetAllPackages";
+import GetAllUsers from "./GetAllUsers";
+
+
+
+export default function Customer()
+{
+    return(
+        <>
+       
+        {/* <GetAllUsers/>
+        <GetAllPackages/> */}
+      {/* <DietHome/> */}
+        </>
+    );
+}
